82d11b2e316d891402724e9d7b0973260c863748
theocld <110028611+theocld@users.noreply.github.com>
2022-08-03 15:42:42 +0200

    Update install_agent.sh

--- install_agent.sh
+++ install_agent.sh
@@ -4,9 +4,9 @@ echo "Importation du binaire agent_rke2"
 curl -sfL https://get.rke2.io | sudo INSTALL_RKE2_TYPE="agent" sh -
 
 echo "Configuration de l'agent node"
-cp config_agent.yaml config.yaml
+cp config_agent config.yaml
 sudo mkdir -p /etc/rancher/rke2
-sudo cp $HOME/config.yaml /etc/rancher/rke2
+sudo cp ~/config.yaml /etc/rancher/rke2
 
 echo "Installation de l'agent"
 sudo systemctl enable rke2-agent.service
