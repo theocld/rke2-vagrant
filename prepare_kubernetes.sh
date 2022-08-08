af31c3792071d387800d8f06d02a0161df5deb48
theocld <110028611+theocld@users.noreply.github.com>
2022-08-08 15:36:08 +0200

    Update prepare_kubernetes.sh

--- prepare_kubernetes.sh
+++ prepare_kubernetes.sh
@@ -6,6 +6,11 @@ sudo chmod +x /usr/local/bin/kubectl
 sudo echo export PATH="/usr/local/bin:$PATH" >> .bashrc
 source ~/.bashrc
 
+echo "Installation helm"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
 echo "Activation du cluster"
 sudo cp /etc/rancher/rke2/rke2.yaml $HOME
 sudo chown $(id -u):$(id -g) $HOME/rke2.yaml
