ae7d63525284aac8040b0ea9e5761d5cc2977316
theocld <110028611+theocld@users.noreply.github.com>
2022-08-16 15:58:58 +0200

    Update install_rancher.sh

--- install_rancher.sh
+++ install_rancher.sh
@@ -6,9 +6,7 @@ kubectl create namespace cattle-system
 
 echo "Installation du gestionnaire de certificat"
 kubectl create namespace cert-manager
-kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.9.1/cert-manager.yaml
-kubectl delete ValidatingWebhookConfiguration --all
-kubectl delete service cert-manager-webhook -n cert-manager
+kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.yaml
 
 echo "Installation de Rancher"
 helm install rancher rancher-stable/rancher  \
