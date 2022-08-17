5339da44ce75f4cb9796253a0a2e3229e8130371
theocld <110028611+theocld@users.noreply.github.com>
2022-08-17 11:01:02 +0200

    Update install_rancher.sh

--- install_rancher.sh
+++ install_rancher.sh
@@ -7,19 +7,6 @@ kubectl create namespace cattle-system
 echo "Installation du gestionnaire de certificat"
 kubectl create namespace cert-manager
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.yaml
-kubectl get -o yaml --all-namespaces \
-issuer,clusterissuer,certificates,certificaterequests > cert-manager-backup.yaml
-helm uninstall cert-manager
-kubectl delete -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.crds.yaml
-kubectl apply --validate=false -f https://github.com/cert-manager/cert-manager/releases/download/v1.7.1/cert-manager.crds.yaml
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-helm install \
-  cert-manager jetstack/cert-manager \
-  --namespace cert-manager \
-  --version v0.12.0
-
-kubectl apply -f cert-manager-backup.yaml
 
 echo "Installation de Rancher"
 helm install rancher rancher-stable/rancher  \
