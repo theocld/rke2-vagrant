81d5b90725684e5005ae413993da4c7707a55fb2
theocld <110028611+theocld@users.noreply.github.com>
2022-09-23 14:14:13 +0200

    Update preliminary_stage_node.sh

--- preliminary_stage_node.sh
+++ preliminary_stage_node.sh
@@ -3,8 +3,8 @@
 echo "Désactivation du apparmor, du firewalld et du swap"
 
 sudo systemctl disable apparmor.service
-sudo systemctl disable firewalld.service
+sudo systemctl stop ufw
+sudo systemctl disable ufw
 sudo systemctl stop apparmor.service
-sudo systemctl stop firewalld.service
 sudo systemctl disable swap.target
 sudo swapoff -a
