c1bd6ae6ba977be0f16279def6b9e2e075fc9dc2
theocld <110028611+theocld@users.noreply.github.com>
2023-02-03 13:47:11 +0100

    Update install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -5,6 +5,14 @@ curl -sfL https://get.rke2.io | sudo sh -
 
 echo "Configuration du server node"
 cp config_server.yaml config.yaml
+prv_ip=$(ip a s eth0 | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)
+pub_ip=$(ip a s eth1 | egrep -o 'inet [0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}' | cut -d' ' -f2)
+sudo cat >> config.yaml << EOF
+tls-san:
+- $prv_ip
+- $pub_ip
+EOF
+
 sudo mkdir -p /etc/rancher/rke2
 sudo cp $HOME/config.yaml /etc/rancher/rke2
 
